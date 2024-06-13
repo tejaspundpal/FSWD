1bbbfceb63d8d5150ab922754c43f8e8eff788f1
tejaspundpal <96496782+tejaspundpal@users.noreply.github.com>
2024-06-13 12:44:24 +0530

    express routing and middleware

---
+++ ExpressRouting/Router/mainRouter.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const routingCompont = express.Router();
+const verifying = require('../middleware/verification');
+
+routingCompont.get("/", verifying, (req, res) => {
+    res.send("This is the get request ");
+})
+routingCompont.get("/about", (req, res) => {
+    res.send("This is the about ");
+})
+// This the commet
+routingCompont.post("/onehand", async (req, res) => {
+    try {
+        const body = req.body;
+
+        console.log(body);
+        res.send({ message: "The body you send is", body });
+    } catch (e) {
+        console.log("The error is", e);
+    }
+});
+routingCompont.put("/thisPutRequest", (req, res) => {
+    res.send({ message: "This is the put request" });
+})
+
+module.exports = routingCompont;
